1ea39b0439a7f304b6c60472945aace0da5eb757
薛焱 <757094197@qq.com>
2016-01-11 17:10:59 +0800

    Segment
    Segment

--- TestSegment/ViewController.m
+++ TestSegment/ViewController.m
@@ -8,7 +8,11 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UIScrollViewDelegate>
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (strong, nonatomic) UIView *lineView;
+@property (weak, nonatomic) IBOutlet UIButton *firstButton;
+@property (weak, nonatomic) IBOutlet UIButton *secondButton;
 
 @end
 
@@ -16,7 +20,34 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.lineView = [[UIView alloc]init];
+    [self.view addSubview:self.lineView];
+    self.lineView.backgroundColor = [UIColor redColor];
+    self.lineView.frame = CGRectMake(CGRectGetMinX(self.firstButton.frame), CGRectGetMaxY(self.firstButton.frame), CGRectGetWidth(self.firstButton.frame), 5);
+    self.scrollView.pagingEnabled = YES;
+    self.scrollView.delegate = self;
+}
+- (IBAction)firstButtonAction:(id)sender {
+    
+    [UIView animateWithDuration:0.5 animations:^{
+        self.lineView.frame = CGRectMake(CGRectGetMinX(self.firstButton.frame), CGRectGetMaxY(self.firstButton.frame), CGRectGetWidth(self.firstButton.frame), 5);
+        
+       self.scrollView.contentOffset = CGPointMake(0, 0);
+    }];
+    
+}
+- (IBAction)secondButtonAction:(id)sender {
+
+    
+    [UIView animateWithDuration:0.5 animations:^{
+        self.lineView.frame = CGRectMake(CGRectGetMinX(self.secondButton.frame), CGRectGetMaxY(self.secondButton.frame), CGRectGetWidth(self.secondButton.frame), 5);
+        self.scrollView.contentOffset = CGPointMake([UIScreen mainScreen].bounds.size.width, 0);
+    }];
+    
+}
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+    self.lineView.frame = CGRectMake(CGRectGetMinX(self.firstButton.frame) + scrollView.contentOffset.x / [UIScreen mainScreen].bounds.size.width * (CGRectGetMinX(self.secondButton.frame) - CGRectGetMinX(self.firstButton.frame)), CGRectGetMaxY(self.firstButton.frame), CGRectGetWidth(self.firstButton.frame), 5);
 }
 
 - (void)didReceiveMemoryWarning {
